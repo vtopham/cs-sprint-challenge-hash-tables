cb5324faf609bdfb27b9a73cf1b9bc4c58ca487d
Victoria  Topham <topham.victoria@gmail.com>
2020-07-10 08:05:38 -0700

    ex4 passed the test

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,18 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    nums = {}
+    #find out if the opposite is in there. if it is, then change value to 1 to indicate that there is a pair. otherwise, add the number as a key.
+    for x in a:
+        if x * -1 not in nums:
+            nums[x] = 0
+        else:
+            nums[x * -1] = 1
+
+    result = []
+    for x in list(nums):
+        if nums[x] == 1:
+            result.append(abs(x))
 
     return result
 
